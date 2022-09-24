40ece4f8a3e927b826338433375ecf7df82cc4f7
reenusivadarshini <78981323+reenusivadarshini@users.noreply.github.com>
2022-09-25 00:09:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,35 +5,27 @@ from PIL import Image
 image = Image.open('header.jpg')
 
 st.write("Welcome")
-hashed_passwords = stauth.Hasher(['123', '456']).generate()
+import streamlit as st
 
-with open('../config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-authenticator = Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
+# Create an empty container
+placeholder = st.empty()
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+actual_email = "email"
+actual_password = "password"
 
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
-    st.title('Some content')
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+# Insert a form in the container
+with placeholder.form("login"):
+    st.markdown("#### Enter your credentials")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submit = st.form_submit_button("Login")
 
-    
-if st.session_state["authentication_status"]:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-elif st.session_state["authentication_status"] == False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] == None:
-    st.warning('Please enter your username and password')
+if submit and email == actual_email and password == actual_password:
+    # If the form is submitted and the email and password are correct,
+    # clear the form/container and display a success message
+    placeholder.empty()
+    st.success("Login successful")
+elif submit and email != actual_email and password != actual_password:
+    st.error("Login failed")
+else:
+    pass
