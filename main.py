9c60434ae06dd46b676c04ce9e2e0bacd0486e89
reenusivadarshini <78981323+reenusivadarshini@users.noreply.github.com>
2022-09-25 01:01:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ from PIL import Image
 
 image = Image.open('header.jpg')
 
-st.write("Welcome")
 import streamlit as st
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://cellularnews.com/wp-content/uploads/2020/03/purple-skys-325x485.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
+add_bg_from_url() 
 # Create an empty container
 placeholder = st.empty()
 
