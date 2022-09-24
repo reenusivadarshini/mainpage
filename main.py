b9292245a861aaca1e467f7c7e4b2ad9a779f130
reenusivadarshini <78981323+reenusivadarshini@users.noreply.github.com>
2022-09-25 00:00:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,36 @@ from PIL import Image
 image = Image.open('header.jpg')
 
 st.write("Welcome")
+hashed_passwords = stauth.Hasher(['123', '456']).generate()
 
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+
+    
+    if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] == False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] == None:
+    st.warning('Please enter your username and password')
